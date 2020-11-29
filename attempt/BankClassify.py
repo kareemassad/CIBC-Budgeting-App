75c2ccf5993e33d63c9aa38fe818ce6063456c9e
Kareem Assad <kareemassad5@gmail.com>
2020-11-28 23:03:35 -0500

    ITS WORKING?!

--- attempt/BankClassify.py
+++ attempt/BankClassify.py
@@ -2,7 +2,6 @@ import re
 import dateutil
 import os
 from datetime import datetime
-
 import pandas as pd
 from textblob.classifiers import NaiveBayesClassifier
 from colorama import init, Fore, Style
@@ -25,26 +24,17 @@ class BankClassify:
             self._get_training(self.prev_data), self._extractor
         )
 
-    def add_data(self, filename, bank="santander"):
+    def add_data(self, filename, bank="cibc"):
         """Add new data and interactively classify it.
         Arguments:
-         - filename: filename of Santander-format file
+         - filename: filename and path of file
         """
-        if bank == "santander":
-            print("adding Santander data!")
-            self.new_data = self._read_santander_file(filename)
-        elif bank == "nationwide":
-            print("adding Nationwide data!")
-            self.new_data = self._read_nationwide_file(filename)
-        elif bank == "lloyds":
-            print("adding Lloyds Bank data!")
-            self.new_data = self._read_lloyds_csv(filename)
-        elif bank == "barclays":
-            print("adding Barclays Bank data!")
-            self.new_data = self._read_barclays_csv(filename)
-        elif bank == "mint":
-            print("adding Mint data!")
-            self.new_data = self._read_mint_csv(filename)
+        if bank == "cibc":
+            print("adding CIBC bank data!")
+            self.new_data = self._read_cibc_csv(filename)
+        # elif bank == "cibc":
+        #     print("adding CIBC bank data!")
+        #     self.new_data = self._read_cibc_csv(filename)
 
         self._ask_with_guess(self.new_data)
 
@@ -76,7 +66,7 @@ class BankClassify:
         """Read list of categories from categories.txt"""
         categories = {}
 
-        with open("categories.txt") as f:
+        with open("attempt/categories.txt") as f:
             for i, line in enumerate(f.readlines()):
                 categories[i] = line.strip()
 
@@ -204,145 +194,30 @@ class BankClassify:
 
         return df
 
-    def _read_santander_file(self, filename):
-        """Read a file in the plain text format that Santander provides downloads in.
-        Returns a pd.DataFrame with columns of 'date', 'desc' and 'amount'."""
-        with open(filename, errors="replace") as f:
-            lines = f.readlines()
-
-        dates = []
-        descs = []
-        amounts = []
-
-        for line in lines[4:]:
-
-            line = "".join(i for i in line if ord(i) < 128)
-            if line.strip() == "":
-                continue
-
-            splitted = line.split(":")
-
-            category = splitted[0]
-            data = ":".join(splitted[1:])
-
-            if category == "Date":
-                dates.append(data.strip())
-            elif category == "Description":
-                descs.append(data.strip())
-            elif category == "Amount":
-                just_numbers = re.sub("[^0-9\.-]", "", data)
-                amounts.append(just_numbers.strip())
-
-        df = pd.DataFrame({"date": dates, "desc": descs, "amount": amounts})
-
-        df["amount"] = df.amount.astype(float)
-        df["desc"] = df.desc.astype(str)
-        df["date"] = df.date.astype(str)
-
-        return df
-
-    def _read_lloyds_csv(self, filename):
-        """Read a file in the CSV format that Lloyds Bank provides downloads in.
-        Returns a pd.DataFrame with columns of 'date' 0 , 'desc'  4 and 'amount' 5 ."""
-
-        df = pd.read_csv(filename, skiprows=0)
-
-        """Rename columns """
-        # df.columns = ['date', 'desc', 'amount']
-        df.rename(
-            columns={
-                "Transaction Date": "date",
-                "Transaction Description": "desc",
-                "Debit Amount": "amount",
-                "Credit Amount": "creditAmount",
-            },
-            inplace=True,
-        )
-
-        # if its income we still want it in the amount col!
-        # manually correct each using 2 cols to create 1 col with either + or - figure
-        # lloyds outputs 2 cols, credit and debit, we want 1 col representing a +- figure
-        for index, row in df.iterrows():
-            if row["amount"] > 0:
-                # it's a negative amount because this is a spend
-                df.at[index, "amount"] = -row["amount"]
-            elif row["creditAmount"] > 0:
-                df.at[index, "amount"] = row["creditAmount"]
-
-        # cast types to columns for math
-        df = df.astype({"desc": str, "date": str, "amount": float})
-
-        return df
-
-    def _read_mint_csv(self, filename) -> pd.DataFrame:
-        """Read a file in the CSV format that mint.intuit.com provides downloads in.
-        Returns a pd.DataFrame with columns of 'date', 'desc', and 'amount'."""
-
-        df = pd.read_csv(filename, skiprows=0)
-
-        """Rename columns """
-        # df.columns = ['date', 'desc', 'amount']
-        df.rename(
-            columns={
-                "Date": "date",
-                "Original Description": "desc",
-                "Amount": "amount",
-                "Transaction Type": "type",
-            },
-            inplace=True,
-        )
-
-        # mint outputs 2 cols, amount and type, we want 1 col representing a +- figure
-        # manually correct amount based on transaction type colum with either + or - figure
-        df.loc[df["type"] == "debit", "amount"] = -df["amount"]
-
-        # cast types to columns for math
-        df = df.astype({"desc": str, "date": str, "amount": float})
-        df = df[["date", "desc", "amount"]]
-
-        return df
-
-    def _read_barclays_csv(self, filename):
-        """Read a file in the CSV format that Barclays Bank provides downloads in.
-        Edge case: foreign txn's sometimes causes more cols than it should
-        Returns a pd.DataFrame with columns of 'date' 1 , 'desc' (memo)  5 and 'amount' 3 ."""
-
-        # Edge case: Barclays foreign transaction memo sometimes contains a comma, which is bad.
-        # Use a work-around to read only fixed col count
-        # https://stackoverflow.com/questions/20154303/pandas-read-csv-expects-wrong-number-of-columns-with-ragged-csv-file
-        # Prevents an error where some rows have more cols than they should
-        temp = pd.read_csv(filename, sep="^", header=None, prefix="X", skiprows=1)
-        temp2 = temp.X0.str.split(",", expand=True)
-        del temp["X0"]
-        df = pd.concat([temp, temp2], axis=1)
-
-        """Rename columns """
-        df.rename(columns={1: "date", 5: "desc", 3: "amount"}, inplace=True)
-
-        # cast types to columns for math
-        df = df.astype({"desc": str, "date": str, "amount": float})
-
-        return df
-
     def _read_cibc_csv(self, filename):
         """Read a file in the CSV format that CIBC Bank provides downloads in.
-        Returns a pd.DataFrame with columns of 'date' 1 , 'desc' (memo)  5 and 'amount' 3 ."""
+        Returns a pd.DataFrame with columns of 'date', 'desc', and 'amount'."""
 
-        # Edge case: Barclays foreign transaction memo sometimes contains a comma, which is bad.
-        # Use a work-around to read only fixed col count
-        # https://stackoverflow.com/questions/20154303/pandas-read-csv-expects-wrong-number-of-columns-with-ragged-csv-file
-        # Prevents an error where some rows have more cols than they should
-        temp = pd.read_csv(filename, sep="^", header=None, prefix="X", skiprows=1)
-        temp2 = temp.X0.str.split(",", expand=True)
-        del temp["X0"]
-        df = pd.concat([temp, temp2], axis=1)
+        data = pd.read_csv(filename)
+        data["Transaction Date"] = pd.to_datetime(data["Transaction Date"])
+        data["Year_Month"] = data["Transaction Date"].dt.strftime("%Y-%m")
+        data["Year_Month"] = pd.to_datetime(data["Year_Month"])
+        data["Year_Month"].min(), data["Year_Month"].max()
+        data["Transaction"] = data["Income"].fillna(0) - data["Expense"].fillna(0)
+        data.drop(columns=["Expense", "Income"])
 
         """Rename columns """
-        df.rename(columns={1: "date", 5: "desc", 3: "amount"}, inplace=True)
+        data.rename(
+            columns={
+                "Description": "desc",
+                "Year_Month": "date",
+                "Transaction": "amount",
+            },
+            inplace=True,
+        )
 
         # cast types to columns for math
-        df = df.astype({"desc": str, "date": str, "amount": float})
-
+        df = data.astype({"desc": str, "date": str, "amount": float})
         return df
 
     def _get_training(self, df):
--- attempt/test.py
+++
@@ -1,5 +0,0 @@
-from BankClassify import BankClassify
-
-bc = BankClassify()
-
-bc.add_data("Statement_Example.txt")
